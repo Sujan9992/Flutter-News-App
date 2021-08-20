eb8736eca1c63c235ff2cd623f7aa4320f633fa4
Sujan Thapa <66910924+Sujan9992@users.noreply.github.com>
2021-08-20 19:12:40 +0545

    Update api.dart

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -6,7 +6,7 @@ class ApiFetch {
   ApiFetch({required this.page});
   Future<List> getNews() async {
     final String url =
-        'https://newsapi.org/v2/everything?apikey=fb1fe456b3af4227b24584a13b567a65&domains=wsj.com&pageSize=10&page=$page&language=en';
+        'https://newsapi.org/v2/everything?apikey=apiKey&domains=wsj.com&pageSize=10&page=$page&language=en';
     var res = await get(
       Uri.parse(url),
     );
